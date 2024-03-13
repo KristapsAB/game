9c6b196001b8c353148e524dc38834d2674da439
KristapsAB <116151065+KristapsAB@users.noreply.github.com>
2024-03-13 18:09:15 +0200

    new
    new

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './style/level.css'; // Import your CSS file
+import Clock from './levelIcon/hourglass(1).png';
+import Poker from './levelIcon/poker.png';
+import Coin from './levelIcon/coin.png';
 
 const Levels = ({ levels }) => {
   return (
     <div className="levels-container">
-   
       <div className="level-grid">
         {levels.map((level, index) => (
-          <div className="level-card" key={index}>
+          <Link
+            key={index}
+            to={`/level/${index + 1}`}
+            state={{ ...level, numberOfCards: level.cardsCount }}
+            className="level-card" // Wrap the entire card
+          >
             <div className="level-header">
               <h2>Level {index + 1}</h2>
-              <p>Cards: {level.cardsCount}</p>
+              <p>
+                <img src={Poker} alt="Poker icon" style={{ width: "20px", height: "20px" }} />
+                &nbsp; {level.cardsCount}
+              </p>
             </div>
             <div className="level-body">
-              <p>Duration: {level.gameDuration} seconds</p>
-              <p>Reward: {level.reward} coins</p>
+              <p>
+                <img
+                  src={Clock}
+                  alt="Clock icon"
+                  style={{ width: "20px", height: "20px", color: "#ffffff" }} // Adjust styles as needed
+                />
+                &nbsp; {level.gameDuration}
+              </p>
+              <p>
+                <img src={Coin} alt="Coin icon" style={{ width: "20px", height: "20px" }} /> {/* Added coin icon */}
+                &nbsp; {level.reward}
+              </p>
             </div>
-            <Link
-              to={`/level/${index + 1}`}
-              state={{ ...level, numberOfCards: level.cardsCount }}
-            >
-              <button>Play Level {index + 1}</button>
-            </Link>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
